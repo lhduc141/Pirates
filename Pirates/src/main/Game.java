d0641ac5e57da19587e87d10b26a83416795e636
cyber2z3 <ititiu21181@student.hcmiu.edu.vn>
2023-03-24 15:55:04 +0700

    Part 1

---
+++ Pirates/src/main/Game.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Game {
+	
+	private GameWindown gameWindown;
+	
+	public Game() {
+		gameWindown = new GameWindown();
+	}
+}
---
+++ Pirates/src/main/MainClass.java
@@ -0,0 +1,8 @@
+package main;
+
+public class MainClass {
+	public static void main(String[] args) {
+		Game game = new Game();
+		new Game();
+	}
+}
