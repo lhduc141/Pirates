db036f6541373be3bb0ad1aa257610ef3002b785
cyber2z3 <cyberzzz2k3@gmail.com>
2023-04-13 16:23:53 +0700

    Animation of characrter

--- Pirates/src/inputs/MouseInputs.java
+++ Pirates/src/inputs/MouseInputs.java
@@ -22,7 +22,6 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-
 	}
 
 	@Override
--- Pirates/src/main/GamePanel.java
+++ Pirates/src/main/GamePanel.java
@@ -12,21 +12,42 @@ import javax.swing.JPanel;
 
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
+
+import static utilz.Constant.PlayerConstants.*;
+import static utilz.Constant.Directions.*;
+
 public class GamePanel extends JPanel {
 
 	private MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100;
 	private BufferedImage img;
+	private BufferedImage[][] animations;
+	private int aniTick, aniIndex, aniSpeed = 15;
+	private int playerAction = IDLE; 
+	private int playerDir = -1;
+	private boolean moving = false;
 
 	public GamePanel() {
 		mouseInputs = new MouseInputs(this);
 
 		importImg();
+		loadAnimations();
 
 		setPanelSize();
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
+
+	}
+
+	private void loadAnimations() {
+		animations = new BufferedImage[9][6]; //creat the Arrray animation 
+
+		for (int j = 0; j < animations.length; j++) {
+			for (int i = 0; i < animations[j].length; i++){
+				animations[j][i] = img.getSubimage(i*64, j*40, 64, 40);
+			}
+		}
 
 	}
 
@@ -48,27 +69,68 @@ public class GamePanel extends JPanel {
 		setMaximumSize(size);
 	}
 
-	public void changeXDelta(int value) {
-		this.xDelta += value;
-
+	public void setDirection(int direction){
+		this.playerDir = direction;
+		moving = true;
 	}
 
-	public void changeYDelta(int value) {
-		this.yDelta += value;
-
+	public void setMoving (boolean moving){
+		this.moving = moving;
 	}
 
-	public void setRectPos(int x, int y) {
-		this.xDelta = x;
-		this.yDelta = y;
+	private void setAnimation() {
+		if(moving ){
+			playerAction = RUNNING;
+		}else 
+			playerAction = IDLE;
 	}
 
+	private void updatePost() {
+
+		if (moving){
+			switch(playerDir){
+				case LEFT:
+					xDelta -=5;
+					break;
+				case RIGHT:
+					xDelta +=5;
+					break;
+				case UP:
+					yDelta -=5;
+					break;
+				case DOWN: 
+					yDelta +=5;
+					break;
+
+		}
+	}
+	}
+
+
+	private void updateAnimationTick() {
+		aniTick ++;
+		if (aniTick >= aniSpeed){
+			aniTick =0; 
+			aniIndex ++;
+			if (aniIndex >= GetSpikeAmount(playerAction)){ 
+				aniIndex = 0;
+			}
+		}
+	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		updateAnimationTick();
 
-		g.drawImage(img.getSubimage(0, 0, 64, 40) , 0, 0, null);
+		setAnimation();	//choose animation: IDLE, RUNNING, JUMP, FALL,.... 
+		updatePost();	//update positive
+
+		g.drawImage(animations[playerAction][aniIndex], (int)xDelta , (int)yDelta , 256,160,  null);
 	}
+
+
+
+
 
 
 }
