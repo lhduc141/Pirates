8b1de550083a45946cc966d91f3d6c4c71e812e5
GIANG NGUYỄN <hoangiang272003@gmail.com>
2023-04-26 13:32:49 +0700

    Menu

--- Pirates/src/gamestates/Playing.java
+++ Pirates/src/gamestates/Playing.java
@@ -1,99 +1,127 @@
 package gamestates;
 
 import java.awt.Graphics;
-import java.awt.RenderingHints.Key;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-
-import javax.swing.JComponent;
 
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
+import ui.PauseOverlay;
 
-public class Playing extends State implements Statemethods{
-    private Player player; 
-    private LevelManager levelManager;
+public class Playing extends State implements Statemethods {
+	private Player player;
+	private LevelManager levelManager;
+	private PauseOverlay pauseOverlay;
+	private boolean paused = false;
 
-    public Playing(Game game){
-        super(game);
-        initClasses();
-    }
+	public Playing(Game game) {
+		super(game);
+		initClasses();
+	}
 
-    private void initClasses() {
-        levelManager = new LevelManager(game);
-        player = new Player(200, 200, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE));
-        player.loadLvlData(levelManager.getCurrentLevel().getLevelData());   
-    }
+	private void initClasses() {
+		levelManager = new LevelManager(game);
+		player = new Player(200, 200, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE));
+		player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
+		pauseOverlay = new PauseOverlay(this);
+	}
 
+	@Override
+	public void update() {
+		if (!paused) {
+			levelManager.update();
+			player.update();
+		} else {
+			pauseOverlay.update();
+		}
+	}
 
-    //IMPLEMENT METHODs
-    @Override
-    public void update() { 
-        levelManager.update();
-        player.update();
-    }
-    public void draw(Graphics g){
-        levelManager.draw(g);
-        player.render(g);
-    }
+	@Override
+	public void draw(Graphics g) {
+		levelManager.draw(g);
+		player.render(g);
 
-    @Override
-    public void mousePressed(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e)  {}
-    public void mouseMoved(MouseEvent e)  {}
-    public void mouseClicked(MouseEvent e) {
-        // if (e.getButton() == MouseEvent.BUTTON1)
-		// 	    player.setAttack(true);
-    }
+		if (paused)
+			pauseOverlay.draw(g);
+	}
 
-    public void keyPressed(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            //key
-            case KeyEvent.VK_A:
-                player.setLeft(true);
-                break;
-            case KeyEvent.VK_D:
-                player.setRight(true);
-                break;
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON1)
+			player.setAttacking(true);
+	}
 
-            //key attack => Hoang Duc Choose J is attack
-            case KeyEvent.VK_J:
-                player.setAttack(true);
-                break;
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_A:
+			player.setLeft(true);
+			break;
+		case KeyEvent.VK_D:
+			player.setRight(true);
+			break;
+		case KeyEvent.VK_SPACE:
+			player.setJump(true);
+			break;
+		case KeyEvent.VK_ESCAPE:
+			paused = !paused;
+			break;
+		}
+	}
 
-            //key jump => Cyber choose K is jump 
-            case KeyEvent.VK_K:
-                player.setJump(true);
-                break;
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_A:
+			player.setLeft(false);
+			break;
+		case KeyEvent.VK_D:
+			player.setRight(false);
+			break;
+		case KeyEvent.VK_SPACE:
+			player.setJump(false);
+			break;
+		}
 
-            //back to menu: original is BacksSpace
-            case KeyEvent.VK_ESCAPE:
-                Gamestate.state = Gamestate.MENU;
-            }
-    }
+	}
 
-    public void keyReleased(KeyEvent e) {
-        switch (e.getKeyCode()){
-			case KeyEvent.VK_A:
-				player.setLeft(false);
-				break;
-			case KeyEvent.VK_D:
-                player.setRight(false); 
-				break;
-            //attack
-			case KeyEvent.VK_J:
-                player.setRight(false);
-				break;
-            //jump
-			case KeyEvent.VK_K:
-                player.setJump(false);
-				break;
-    }
-    }
+	public void mouseDragged(MouseEvent e) {
+		if (paused)
+			pauseOverlay.mouseDragged(e);
+	}
 
-    public Player getPlayer(){
-        return player;
-    }
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (paused)
+			pauseOverlay.mousePressed(e);
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		if (paused)
+			pauseOverlay.mouseReleased(e);
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		if (paused)
+			pauseOverlay.mouseMoved(e);
+
+	}
+
+	public void unpauseGame() {
+		paused = false;
+	}
+
+	public void windowFocusLost() {
+		player.resetDirBooleans();
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
 
 }
--- Pirates/src/inputs/KeyboardInputs.java
+++ Pirates/src/inputs/KeyboardInputs.java
@@ -17,34 +17,34 @@ public class KeyboardInputs implements KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		switch (Gamestate.state){
-			case MENU:
-				gamePanel.getGame().getMenu().keyReleased(e);
-				break;
-			case PLAYING:
-				gamePanel.getGame().getPlaying().keyReleased(e);
-				break;
-			default:
-				break;
-			}
+		switch (Gamestate.state) {
+		case MENU:
+			gamePanel.getGame().getMenu().keyReleased(e);
+			break;
+		case PLAYING:
+			gamePanel.getGame().getPlaying().keyReleased(e);
+			break;
+		default:
+			break;
+
+		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		switch (Gamestate.state){
-			case MENU:
-				gamePanel.getGame().getMenu().keyPressed(e);
-				break;
-			case PLAYING:
-				gamePanel.getGame().getPlaying().keyPressed(e);
-				break;
-			default:
-				break;}
+		switch (Gamestate.state) {
+		case MENU:
+			gamePanel.getGame().getMenu().keyPressed(e);
+			break;
+		case PLAYING:
+			gamePanel.getGame().getPlaying().keyPressed(e);
+			break;
+		default:
+			break;
+		}
 	}
-
 }
