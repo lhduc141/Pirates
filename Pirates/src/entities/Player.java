f801f5476d05e06c9eb45cf39f0b986e28e001d5
GIANG NGUYỄN <hoangiang272003@gmail.com>
2023-04-27 09:42:15 +0700

    draw hit box

--- Pirates/src/entities/Player.java
+++ Pirates/src/entities/Player.java
@@ -41,7 +41,7 @@ public class Player extends Entity {
 
 	public void render(Graphics g) {
 		g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset), (int) (hitbox.y - yDrawOffset), width, height, null);
-//		drawHitbox(g);
+		drawHitbox(g);
 	}
 
 	private void updateAnimationTick() {
