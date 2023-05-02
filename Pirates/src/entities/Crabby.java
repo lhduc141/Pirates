7222d29bba72fa327b320c9d6374ed3b05c0c090
cyber141 <cyberzzz2k3@gmail.com>
2023-05-02 10:52:03 +0700

    update position of crab

--- Pirates/src/entities/Crabby.java
+++ Pirates/src/entities/Crabby.java
@@ -1,12 +1,12 @@
 package entities;
 
 import static utilz.Constants.EnemyConstants.*;
-
+import main.Game;
 public class Crabby extends Enemy {
 
     public Crabby(float x, float y) {
         super(x, y, CRABBY_WIDTH, CRABBY_HEIGHT, CRABBY);
-        //TODO Auto-generated constructor stub
+        initHitbox(x, y, (int) (22*Game.SCALE), (int) (19*Game.SCALE));
     }
     
 }
--- Pirates/src/entities/Enemy.java
+++ Pirates/src/entities/Enemy.java
@@ -1,10 +1,17 @@
 package entities;
 
 import static utilz.Constants.EnemyConstants.*;
+import static utilz.HelpMethods.*;
+
+import main.Game;
 
 public abstract class Enemy extends Entity {
     private int aniIndex, enemyState, enemyType;
     private int aniTick, aniSpeed = 25;
+    private boolean firstUpdate = true;
+    private boolean inAir ; 
+    private float fallSpeed;
+    private float gravity = 0.04f * Game.SCALE;
 
     public Enemy(float x, float y, int width, int height, int enemyType) {
             super(x, y, width, height);
@@ -23,8 +30,27 @@ public abstract class Enemy extends Entity {
         }
     }
 
-    public void update(){
+    public void update(int[][] lvlData){
+        updateMove(lvlData);
         updateAnimationTick();
+    }
+
+    private void updateMove(int[][] lvlData){
+        if (firstUpdate)
+            if (!IsEntityOnFloor(hitbox, lvlData))
+                inAir = true; 
+        if (inAir){
+            if(CanMoveHere(hitbox.x, hitbox.y + fallSpeed, hitbox.width, hitbox.height, lvlData)){
+                hitbox.y += fallSpeed;
+                fallSpeed += gravity; 
+            }else {
+                inAir  = false; 
+                hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, fallSpeed); 
+            }
+
+        }else {
+
+        }
     }
 
     public int getAniIndex(){
