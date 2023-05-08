39b25879f26dbe018fad36cd8498f631dd5b3847
luan <latla21@gnail.com>
2023-05-08 12:00:06 +0700

    add constant values for cannon

--- Pirates/src/utilz/Constants.java
+++ Pirates/src/utilz/Constants.java
@@ -14,6 +14,8 @@ public class Constants {
 		public static final int BARREL = 2;
 		public static final int BOX = 3;
 		public static final int SPIKE = 4;
+		public static final int CANNON_LEFT = 5;
+		public static final int CANNON_RIGHT = 6;
 
 		public static final int RED_POTION_VALUE = 15;
 		public static final int BLUE_POTION_VALUE = 10;
@@ -33,12 +35,19 @@ public class Constants {
 		public static final int SPIKE_WIDTH = (int) (Game.SCALE * SPIKE_WIDTH_DEFAULT);
 		public static final int SPIKE_HEIGHT = (int) (Game.SCALE * SPIKE_HEIGHT_DEFAULT);
 
+		public static final int CANNON_WIDTH_DEFAULT = 40;
+		public static final int CANNON_HEIGHT_DEFAULT = 26;
+		public static final int CANNON_WIDTH = (int)(CANNON_WIDTH_DEFAULT * Game.SCALE);
+		public static final int CANNON_HEIGHT = (int)(CANNON_HEIGHT_DEFAULT * Game.SCALE);
+
 		public static int GetSpriteAmount(int object_type){
 			switch (object_type){
 				case RED_POTION, BLUE_POTION:
 					return 7;
 				case BARREL, BOX:
 					return 8;
+				case CANNON_LEFT, CANNON_RIGHT:
+					return 7;
 			}
 
 			return 1;
