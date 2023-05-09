6dc244ce04bfb830b839be8509e986370ea694e3
luan <latla21@gnail.com>
2023-05-09 23:13:21 +0700

    add AudioOptions when pause game

--- Pirates/src/main/Game.java
+++ Pirates/src/main/Game.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import gamestates.Gamestate;
 import gamestates.Menu;
 import gamestates.Playing;
+import ui.AudioOptions;
 import utilz.LoadSave;
 
 public class Game implements Runnable {
@@ -16,6 +17,7 @@ public class Game implements Runnable {
 
 	private Playing playing;
 	private Menu menu;
+	private AudioOptions audioOptions;
 
 	public final static int TILES_DEFAULT_SIZE = 32;
 	public final static float SCALE = 2f;
@@ -38,6 +40,7 @@ public class Game implements Runnable {
 	}
 
 	private void initClasses() {
+		audioOptions = new AudioOptions();
 		menu = new Menu(this);
 		playing = new Playing(this);
 	}
@@ -133,5 +136,9 @@ public class Game implements Runnable {
 
 	public Playing getPlaying() {
 		return playing;
+	}
+
+	public AudioOptions getAudioOptions() {
+		return audioOptions;
 	}
 }
