b2fc100cf6ec15efcbec250a599bd13dfff92f03
luan <latla21@gnail.com>
2023-05-09 19:20:45 +0700

    add death screen when die

--- Pirates/src/ui/GameOverOverlay.java
+++ Pirates/src/ui/GameOverOverlay.java
@@ -3,6 +3,7 @@ package ui;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 import gamestates.Gamestate;
 import gamestates.Playing;
@@ -11,17 +12,32 @@ import main.Game;
 public class GameOverOverlay {
 
     private Playing playing;
+    private BufferedImage img;
+    private int imgX, imgY, imgW, imgH;
+
     public GameOverOverlay (Playing playing){
         this.playing = playing;
+        createImg();
     }
     
+    private void createImg() {
+        img = LoadSave.GetSpriteAtlas(LoadSave.DEATH_SCREEN);
+        imgW = (int)(img.getWidth() * Game.SCALE);
+        imgH = (int)(img.getHeight() * Game.SCALE);
+        imgX = Game.GAME_WIDTH/2 - imgW/2;
+        imgY = (int)(100*Game.SCALE);
+    }
+
     public void draw(Graphics g){
         g.setColor(new Color(0, 0, 0, 200));
         g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
 
-        g.setColor(Color.white);
-        g.drawString("NGUUUUUUU", Game.GAME_WIDTH/2, 150);
-        g.drawString("PRESS ESC TO ENTER MAIN MENU", Game.GAME_WIDTH/2, 300);
+        g.drawImage(img, imgX, imgY, imgW, imgH, null);
+
+
+        // g.setColor(Color.white);
+        // g.drawString("NGUUUUUUU", Game.GAME_WIDTH/2, 150);
+        // g.drawString("PRESS ESC TO ENTER MAIN MENU", Game.GAME_WIDTH/2, 300);
 
     }
 
