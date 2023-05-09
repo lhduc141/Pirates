c69835f9c52809a0752bcb55744245de0a43645a
luan <latla21@gnail.com>
2023-05-10 00:13:38 +0700

    add option image

--- Pirates/src/main/Game.java
+++ Pirates/src/main/Game.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.awt.Graphics;
+
+import gamestates.GameOptions;
 import gamestates.Gamestate;
 import gamestates.Menu;
 import gamestates.Playing;
@@ -17,6 +19,7 @@ public class Game implements Runnable {
 
 	private Playing playing;
 	private Menu menu;
+	private GameOptions gameOptions;
 	private AudioOptions audioOptions;
 
 	public final static int TILES_DEFAULT_SIZE = 32;
@@ -43,6 +46,7 @@ public class Game implements Runnable {
 		audioOptions = new AudioOptions();
 		menu = new Menu(this);
 		playing = new Playing(this);
+		gameOptions = new GameOptions(this);
 	}
 
 	private void startGameLoop() {
@@ -59,6 +63,8 @@ public class Game implements Runnable {
 			playing.update();
 			break;
 		case OPTIONS:
+		gameOptions.update();
+		break;
 		case QUIT:
 		default:
 			System.exit(0);
@@ -74,6 +80,9 @@ public class Game implements Runnable {
 			break;
 		case PLAYING:
 			playing.draw(g);
+			break;
+			case OPTIONS:
+			gameOptions.draw(g);
 			break;
 		default:
 			break;
@@ -136,6 +145,10 @@ public class Game implements Runnable {
 
 	public Playing getPlaying() {
 		return playing;
+	}
+
+	public GameOptions getGameOptions() {
+		return gameOptions;
 	}
 
 	public AudioOptions getAudioOptions() {
