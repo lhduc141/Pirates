a3e51ad3fc5d1279327c7327afbfcb9c10a8f35e
luan <latla21@gnail.com>
2023-05-11 12:36:42 +0700

    add getClip method

--- Pirates/src/audio/AudioPlayer.java
+++ Pirates/src/audio/AudioPlayer.java
@@ -1,8 +1,15 @@
 package audio;
 
+import java.io.IOException;
+import java.net.URL;
+import java.rmi.server.LoaderHandler;
 import java.util.Random;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class AudioPlayer {
     
@@ -25,6 +32,40 @@ public class AudioPlayer {
 	private Random rand = new Random();
     
     public AudioPlayer() {
+        loadSongs();
+        loadEffects();
+    }
 
+    private void loadSongs() {
+        String[] names = {"menu","level1","level2"};
+        songs = new Clip[names.length];
+        for (int i = 0; i < songs.length; i++) {
+            songs[i] = getClip(names[i]);
+        }
+    }
+
+    private void loadEffects() {
+        String[] effectNames = {"die","jump","gameover","lvlcompleted","attck1","attack2","attack3"};
+        effects = new Clip[effectNames.length];
+        for (int i = 0; i < effects.length; i++) {
+            effects[i] = getClip(effectNames[i]);
+        }
+    }
+
+    private Clip getClip(String name) {
+        URL url = getClass().getResource("/audio/" + name + ".wav");
+        AudioInputStream audio;
+
+        try {
+            audio = AudioSystem.getAudioInputStream(url);
+            Clip c = AudioSystem.getClip();
+        c.open(audio);
+        return c;
+        
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            
+            e.printStackTrace();
+        }
+        return null;
     }
 }
